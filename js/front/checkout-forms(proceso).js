45ac185f746e1a12d1c18b87db3530180fea5ba7
munyecodiabolico <dissenyramon@gmail.com>
2023-01-13 15:45:34 +0100

    final guardado en tabla emails

---
+++ js/front/checkout-forms(proceso).js
@@ -0,0 +1,55 @@
+import { validateForm } from './validation3.js';
+
+export let renderForms = () => { 
+
+    let submitForm = document.querySelector('#submitForm');
+
+    if (submitForm) {
+            
+        submitForm.addEventListener('click', event => {
+    
+            event.preventDefault();
+
+            let form = document.querySelector('.checkout-form');
+
+            if(!validateForm(form.elements)){
+                return;
+            };
+
+            let url = form.action;
+            let formData = new FormData(form);
+            let formDataJson = Object.fromEntries(formData.entries());
+            console.log(url);
+            console.log(formDataJson);
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + sessionStorage.getItem('accessToken')
+                },
+                body: JSON.stringify(formDataJson)
+            }).then(response => {
+                return response.json();
+            }).then(data => {
+                document.dispatchEvent(new CustomEvent('message', {
+                    detail: {
+                        text: 'Formulario enviado correctamente',
+                        type: 'success'
+                    }
+                }));
+            }).catch(error => {
+                document.dispatchEvent(new CustomEvent('message', {
+                    detail: {
+                        text: 'Fallo al enviar el formulario',
+                        type: 'error'
+                    }
+                }));
+            });
+
+        });
+
+    };
+
+}
+
+    
