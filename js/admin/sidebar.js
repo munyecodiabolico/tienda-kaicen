50b8545d4b2a2b6a79dfedaac379c77f5925037f
munyecodiabolico <dissenyramon@gmail.com>
2023-01-24 20:01:38 +0100

    sidebar pasa titulo pagina (proceso)

--- js/admin/sidebar.js
+++ js/admin/sidebar.js
@@ -1,7 +1,7 @@
 export let renderSidebar = () => {
     // Despliegue menu lateral y boton hamburguesa
     let adminPanel = document.querySelector(".admin-panel");
-    let sideBar = document.querySelector(".sidebar-menu");
+    let sideBar = document.querySelector(".container-sidebar");
     let burger = document.querySelector(".hamburger");
     if (burger) {
         burger.addEventListener('click', () => {
@@ -15,53 +15,53 @@ export let renderSidebar = () => {
 
 
 
-    let listElements = document.querySelectorAll(".list-button");
-    let listElementsSubmenus = document.querySelectorAll(".list-inside");
+    // let listElements = document.querySelectorAll(".list-button");
+    // let listElementsSubmenus = document.querySelectorAll(".list-inside");
 
-    listElements.forEach(listElement => {
-        listElement.addEventListener('click', (event) => {
-            event.preventDefault();
-            let listElements = document.querySelectorAll(".list-button");
-            listElements.forEach(listElement => {
-                event.preventDefault();
-                listElement.classList.remove("arrow");
-                listElement.classList.remove("active");
-                if (listElement.classList.contains("list-button-click")) {
-                    let menu = listElement.nextElementSibling;
-                    menu.style.height = "0";
-                }
-            });
-            listElementsSubmenus.forEach(listElementSubmenu => {
-                listElementSubmenu.classList.remove("active");
-            });
-            if (listElement.classList.contains("list-button-click")) {
-                listElement.classList.toggle("arrow");
-                listElement.classList.toggle("active");
-                let height = 0;
-                let menu = listElement.nextElementSibling;
-                if (menu.clientHeight == "0") {
-                    height = menu.scrollHeight;
-                } else {
-                    listElement.classList.toggle("arrow");
-                    listElement.classList.toggle("active");
-                }
-                menu.style.height = `${height}px`;
-            } else {
-                listElement.classList.toggle("active");
-            }
-        });
+    // listElements.forEach(listElement => {
+    //     listElement.addEventListener('click', (event) => {
+    //         event.preventDefault();
+    //         let listElements = document.querySelectorAll(".list-button");
+    //         listElements.forEach(listElement => {
+    //             event.preventDefault();
+    //             listElement.classList.remove("arrow");
+    //             listElement.classList.remove("active");
+    //             if (listElement.classList.contains("list-button-click")) {
+    //                 let menu = listElement.nextElementSibling;
+    //                 menu.style.height = "0";
+    //             }
+    //         });
+    //         listElementsSubmenus.forEach(listElementSubmenu => {
+    //             listElementSubmenu.classList.remove("active");
+    //         });
+    //         if (listElement.classList.contains("list-button-click")) {
+    //             listElement.classList.toggle("arrow");
+    //             listElement.classList.toggle("active");
+    //             let height = 0;
+    //             let menu = listElement.nextElementSibling;
+    //             if (menu.clientHeight == "0") {
+    //                 height = menu.scrollHeight;
+    //             } else {
+    //                 listElement.classList.toggle("arrow");
+    //                 listElement.classList.toggle("active");
+    //             }
+    //             menu.style.height = `${height}px`;
+    //         } else {
+    //             listElement.classList.toggle("active");
+    //         }
+    //     });
 
-    });
+    // });
 
-    listElementsSubmenus.forEach(listElementSubmenu => {
-        listElementSubmenu.addEventListener('click', (event) => {
-            event.preventDefault();
-            listElementsSubmenus.forEach(listElementSubmenu => {
-                listElementSubmenu.classList.remove("active");
-            });
-            listElementSubmenu.classList.toggle("active");
-        });
-    });
+    // listElementsSubmenus.forEach(listElementSubmenu => {
+    //     listElementSubmenu.addEventListener('click', (event) => {
+    //         event.preventDefault();
+    //         listElementsSubmenus.forEach(listElementSubmenu => {
+    //             listElementSubmenu.classList.remove("active");
+    //         });
+    //         listElementSubmenu.classList.toggle("active");
+    //     });
+    // });
 
 
 
